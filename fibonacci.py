09108636ed110d934651a39d5110670405cdaf99
Hao Lee <haobillylee@gmail.com>
2017-06-14 09:24:22 -0400

    C4: Improved the function fib to be O(n) time

--- fibonacci.py
+++ fibonacci.py
@@ -2,7 +2,16 @@ def fib(N):
 	if N <=1:
 		return N
 	else:
-		return fib(N-1)+fib(N-2)
+		n=1
+		m1=0
+		m2=1
+		m3=1
+		while n != N:
+			m1 = m2
+			m2 = m3
+			m3 = m1+m2
+			n+=1
+		return m3
 
 if __name__ == "__main__":
 	print("Fibo(5)",fib(5))
